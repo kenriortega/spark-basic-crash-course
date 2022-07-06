656c6741d4900a7be1396df36f78dd4bec3b4c28
kenriortega <kenriortega@gmail.com>
2022-07-06 14:06:43 -0400

    start : working with df , implicits , and basics

---
+++ src/main/scala/part2df/DataFrameBasics.scala
@@ -0,0 +1,74 @@
+package part2df
+
+import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.sql.types.{DoubleType, LongType, StringType, StructField, StructType}
+
+object DataFrameBasics extends App {
+  // creating a SparkSession
+  val spark = SparkSession.builder()
+    .appName("DataFrame Basics")
+    .config("spark.master", "local")
+    .getOrCreate()
+
+  // reading DF
+  val firstDf = spark.read
+    .format("json")
+    .option("inferSchema", "true") // in production not be useful
+    .load("src/main/resources/data/cars.json")
+
+  firstDf.show()
+  firstDf.printSchema()
+
+  // get rows
+  firstDf.take(10).foreach(println)
+
+  //  spark types
+  val longType = LongType
+
+  // schema best practices
+  val carsSchema = StructType(Array(
+    StructField("Name", StringType),
+    StructField("Miles_per_Gallon", DoubleType),
+    StructField("Cylinders", LongType),
+    StructField("Displacement", DoubleType),
+    StructField("Horsepower", LongType),
+    StructField("Weight_in_lbs", LongType),
+    StructField("Acceleration", DoubleType),
+    StructField("Year", StringType),
+    StructField("Origin", StringType)
+  ))
+
+  val carsDFSchema = firstDf.schema
+  // read df with your schema
+  val carsDFWithSchema = spark.read
+    .format("json")
+    .schema(carsDFSchema)
+    .load("src/main/resources/data/cars.json")
+
+  val myRow = ("chevrolet chevelle malibu", 18, 8, 307, 130, 3504, 12.0, "1970-01-01", "USA")
+  // create Df from tuples
+  val cars = Seq(
+    ("chevrolet chevelle malibu", 18, 8, 307, 130, 3504, 12.0, "1970-01-01", "USA"),
+    ("buick skylark 320", 15, 8, 350, 165, 3693, 11.5, "1970-01-01", "USA"),
+    ("plymouth satellite", 18, 8, 318, 150, 3436, 11.0, "1970-01-01", "USA"),
+    ("amc rebel sst", 16, 8, 304, 150, 3433, 12.0, "1970-01-01", "USA"),
+    ("ford torino", 17, 8, 302, 140, 3449, 10.5, "1970-01-01", "USA"),
+    ("ford galaxie 500", 15, 8, 429, 198, 4341, 10.0, "1970-01-01", "USA"),
+    ("chevrolet impala", 14, 8, 454, 220, 4354, 9.0, "1970-01-01", "USA"),
+    ("plymouth fury iii", 14, 8, 440, 215, 4312, 8.5, "1970-01-01", "USA"),
+    ("pontiac catalina", 14, 8, 455, 225, 4425, 10.0, "1970-01-01", "USA"),
+    ("amc ambassador dpl", 15, 8, 390, 190, 3850, 8.5, "1970-01-01", "USA")
+  )
+
+  val manualCarsDF = spark.createDataFrame(cars) // schema auto-inferred
+  // note: DFs have schemas, rows do not
+  // create df with implicits
+
+  import spark.implicits._
+
+  val manualCarsDFWithImplicits =
+    cars.toDF("Name", "MPG", "Cylinders", "Displacement", "HP", "Weight", "Acceleration", "Year", "CountryOrigin")
+
+  manualCarsDF.printSchema()
+  manualCarsDFWithImplicits.printSchema()
+}
